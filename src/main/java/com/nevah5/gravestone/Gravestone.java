4bd836e75026bee01434c260a5d87a89363c2bff
Nevah5 <neevaah5@gmail.com>
2022-12-11 22:43:01 +0100

    Added retrieving gravestone interactions

--- src/main/java/com/nevah5/gravestone/Gravestone.java
+++ src/main/java/com/nevah5/gravestone/Gravestone.java
@@ -1,19 +1,19 @@
 package com.nevah5.gravestone;
 
 import org.bukkit.Bukkit;
+import org.bukkit.Material;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.block.Action;
 import org.bukkit.event.entity.PlayerDeathEvent;
+import org.bukkit.event.player.PlayerInteractEvent;
 import org.bukkit.inventory.ItemStack;
 import org.bukkit.plugin.java.JavaPlugin;
 import org.bukkit.event.Listener;
 
-import java.util.ArrayList;
-import java.util.HashMap;
-import java.util.List;
-import java.util.UUID;
+import java.util.*;
 
 public class Gravestone extends JavaPlugin implements Listener{
-    HashMap<String, GravestoneDeath> gravestones = new HashMap<>();
+    private HashMap<String, GravestoneDeath> gravestones = new HashMap<>();
 
     @Override
     public void onEnable(){
@@ -32,5 +32,24 @@ public class Gravestone extends JavaPlugin implements Listener{
 
         GravestoneDeath gravestoneDeath = new GravestoneDeath(drops, x, y, z, uuid);
         gravestones.put(gravestoneDeath.getLocationString(), gravestoneDeath);
+    }
+
+    @EventHandler
+    public void onInteractEvent(PlayerInteractEvent playerInteractEvent){
+        if(!playerInteractEvent.getAction().equals(Action.RIGHT_CLICK_BLOCK)) return;
+        // check if gravestone
+        if(Objects.requireNonNull(playerInteractEvent.getClickedBlock()).getType() != Material.BEDROCK) return;
+        int x = playerInteractEvent.getClickedBlock().getX();
+        int y = playerInteractEvent.getClickedBlock().getY();
+        int z = playerInteractEvent.getClickedBlock().getZ();
+        String key = x+"."+y+"."+z;
+
+        GravestoneDeath gravestone = gravestones.get(key);
+        if(gravestone == null) return;
+        if(gravestone.getUuid() != playerInteractEvent.getPlayer().getUniqueId()) return;
+
+        // player is owner of gravestone
+        //give items to owner
+        Bukkit.broadcastMessage(gravestone.getItems().toString());
     }
 }
